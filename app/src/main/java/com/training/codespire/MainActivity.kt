d4dbe1048ffd9d1ff9452c912b99d1a219fae43d
ahmed <ahmedrapa91@gmail.com>
2024-07-02 15:11:47 +0300

    splash screen

--- app/src/main/java/com/training/codespire/MainActivity.kt
+++ app/src/main/java/com/training/codespire/MainActivity.kt
@@ -2,14 +2,15 @@ package com.training.codespire
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.training.codespire.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(binding.root)
 
     }
