8602160f4c04df01044194128f7659e9bb7a0c4a
ahmed <ahmedrapa91@gmail.com>
2024-07-04 13:41:39 +0300

    make authentication design

--- app/src/main/java/com/training/codespire/AuthActivity.kt
+++ app/src/main/java/com/training/codespire/AuthActivity.kt
@@ -3,14 +3,17 @@ package com.training.codespire
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.training.codespire.databinding.ActivityMainBinding
+import com.training.codespire.databinding.ActivityAuthBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_auth)
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/training/codespire/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.training.codespire
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.training.codespire.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+
+
+
+
+
+
+
+        return binding.root
+    }
+
+}
