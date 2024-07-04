5a65bdb8d51e5c033c5f5e28462fbb22cbfee93d
Ahmed2 <ahmedrapa91@gmail.com>
2024-07-04 16:03:07 +0300

    design the login screen

--- app/src/main/java/com/training/codespire/LoginFragment.kt
+++ app/src/main/java/com/training/codespire/LoginFragment.kt
@@ -19,9 +19,41 @@ class LoginFragment : Fragment() {
 
 
 
+        checkLogin()
+
+
 
 
         return binding.root
     }
+
+    private fun checkLogin() {
+        binding.btnSignIn.setOnClickListener {
+            if (binding.etEmail.text.isEmpty()) {
+                binding.etEmail.error = "Email is required"
+                binding.etEmail.setBackgroundResource(R.drawable.et_border_error)
+                binding.etEmail.setCompoundDrawablesWithIntrinsicBounds(
+                    R.drawable.ic_error_message, 0, 0, 0
+                )
+            } else
+                binding.etEmail.setBackgroundResource(R.drawable.et_border_selector)
+            binding.etEmail.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_password_selector, 0, 0, 0
+            )
+        }
+        if (binding.etPassword.text.isEmpty()) {
+            binding.etPassword.error = "Password is required"
+            binding.etPassword.setBackgroundResource(R.drawable.et_border_error)
+            binding.etPassword.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_error_password, 0, 0, 0
+            )
+        } else {
+            binding.etPassword.setBackgroundResource(R.drawable.et_border_selector)
+            binding.etPassword.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_password_selector, 0, 0, 0
+            )
+        }
+    }
+
 
 }
