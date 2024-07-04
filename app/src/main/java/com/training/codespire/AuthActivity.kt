2bbf4c0a333894f3c2f443f718c44f6303c49e5e
Ahmed2 <ahmedrapa91@gmail.com>
2024-07-04 16:38:16 +0300

    login screen

--- app/src/main/java/com/training/codespire/AuthActivity.kt
+++ app/src/main/java/com/training/codespire/AuthActivity.kt
@@ -1,5 +1,6 @@
 package com.training.codespire
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,6 +9,7 @@ import com.training.codespire.databinding.ActivityAuthBinding
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         installSplashScreen()
         setContentView(R.layout.activity_auth)
 
